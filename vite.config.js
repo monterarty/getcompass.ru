e396b6fbef19364e0598a2e30a44a07edb41f592
Viacheslav Varov <monterarty@gmail.com>
2024-09-27 13:26:45 +0400

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,20 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/getcompass.ru/',  // Указываем базовый путь для GitHub Pages
-
     build: {
-        minify: 'terser', // Использование Terser для минификации
+        minify: 'terser',
         terserOptions: {
             compress: {
-                drop_console: true, // Удалить консольные сообщения из билда
-                drop_debugger: true // Удалить отладочные операторы
-            }
+                drop_console: true,
+                drop_debugger: true,
+            },
         },
         rollupOptions: {
             output: {
-                // Задаем имя для основного файла
                 entryFileNames: `assets/main-v2.6.6.min.js`,
-                // Задаем имя для чанк файлов
                 chunkFileNames: `assets/[name].[hash].min.js`,
-                // Для статических ресурсов
                 assetFileNames: `assets/[name].[hash][extname]`,
-            }
-        }
-    }
+            },
+        },
+    },
 });
